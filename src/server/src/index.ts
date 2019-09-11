29c966c03231ecaa4a126fd22eeefa51715068ba
Raul Alvarez <ra.lvarez11418@gmail.com>
2019-09-11 18:24:36 -0400

    mostly working front end and backend

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,18 +1,16 @@
 import express, { Request, Response } from 'express';
-import channels from './routes/channels';
+import videoRoutes from './routes/videoRoutes';
 
 const app = express();
 
-// app.use()
+app.use(express.json());
 
-// app.use('/', (req: Request, res: Response) => res.sendStatus(200));
-
-app.use('/', channels);
+app.use('/api', videoRoutes);
 
 app.listen('3001', (err: Error) => {
   if (err) {
     throw err;
   }
 
-  console.log(`Server is listening on http://localhost:3000`);
+  console.log(`Server is listening on http://localhost:3001`);
 });
